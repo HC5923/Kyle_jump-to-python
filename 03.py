e1a5170e207fd01e2a37161ac60549c2f89c0f48
HC5923 <5625923@naver.com>
2024-10-08 18:44:20 +0900

    -

--- 03.py
+++ 03.py
@@ -1,3 +1,47 @@
+'''
 # print("hi")
 
 print("hi TSS2 Team")
+'''
+
+'''
+test_list = ['one', 'two', 'three']
+for i in test_list:
+    print(i)
+'''
+
+'''
+money = True
+if money:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+'''
+
+'''
+money = 2000
+card = True
+if money >= 3000 or card:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+'''
+
+'''
+pocket = ['paper', 'cellphone', 'money']
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+'''
+
+# 다양한 조건을 판단하는 elif
+pocket = ['paper', 'cellphone']
+card = True
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    if card:
+        print("택시를 타고 가라")
+    else:
+        print("걸어가라"
