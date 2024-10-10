686e7dca76d1a56fb79bb451a8a27bebb4fd498f
HC5923 <5625923@naver.com>
2024-10-10 10:20:12 +0900

    -

--- 03.py
+++ 03.py
@@ -35,6 +35,7 @@ else:
     print("걸어 가라")
 '''
 
+'''
 # 다양한 조건을 판단하는 elif
 pocket = ['paper', 'cellphone']
 card = True
@@ -44,4 +45,22 @@ else:
     if card:
         print("택시를 타고 가라")
     else:
-        print("걸어가라"
+        print("걸어가라")
+'''
+
+# Q1 조건문의 참과 거짓
+"""
+A1 shirt
+* 3,4 가 참인데 3번이 우선이라 3번에서 컷
+"""
+"""
+a = "Life is too short, you need python"
+
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a: print("python")
+elif "shirt" not in a: print("shirt")
+elif "need" in a: print("need")
+else: print("none")
+"""
+
+# Q2
